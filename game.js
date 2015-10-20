91c2caf42875c65d610632fc0cbb0c23edc9b4e2
Scott Warner <scottbwarner@gmail.com>
2015-10-20 16:44:12 -0400

    make game into an object class

--- game.js
+++ game.js
@@ -1,67 +1,70 @@
-var content = $("#content"),           //select relevant elements
+var content = $("#content"), //select relevant elements
     gameWordContainer = $("#gameword"),
     guessedContainer = $("#guessed"),
-    letterContainer =  $("#letter"),
-    gallowsContainer =  $("#gallows"),
-    feedbackContainer =  $("#feedback"),
+    letterContainer = $("#letter"),
+    gallowsContainer = $("#gallows"),
+    feedbackContainer = $("#feedback"),
     submitBtn = $("#submit");
 
 
+function Game(gameWord) {
+    this.gameWord = gameWord;
+    var wrongLetters = [];
+    var correctLetters = [];
+    var strikes = 0;
+    var displayBlanks = function(word) {
+        var blanks = [];
+        for (var i = 0; i < word.length; i++) {
+            blanks.push("-");
+        }
+        for (var letter in correctLetters) {
+            if (word.indexOf(correctLetters[letter]) > -1) {
+                blanks[word.indexOf(correctLetters[letter])] = correctLetters[letter];
+            }
+        }
+        gameWordContainer.text(blanks.join(""));
+    };
 
-
-var gameWord = "ruby";
-displayBlanks();
-
-var wrongLetters = [];
-var correctLetters = [];
-var strikes = 0;
-
-
-function displayBlanks(){
-var blanks = [];
-for(var i = 0;i < gameWord.length; i++){
-    blanks.push("-");
-  }
-for(var letter in correctLetters){
-  if(gameWord.indexOf(correctLetters[letter]) > -1){
-    blanks[gameWord.indexOf(correctLetters[letter])] = correctLetters[letter];
-  }
-  }
-gameWordContainer.text(blanks.join(""));
+    this.guess = function(letter, word) {
+        if (word.indexOf(letter) > -1) {
+            feedbackContainer.css({
+                "color": "blue"
+            });
+            feedbackContainer.text("You got it:  " + letter);
+            correctLetters.push(letter);
+            displayBlanks(word);
+        } else {
+            feedbackContainer.css({
+                "color": "red"
+            });
+            feedbackContainer.text("Nope, not:  " + letter);
+            wrongLetters.push(letter);
+            strikes += 1;
+            gallowsContainer.text(strikes);
+            guessedContainer.text(wrongLetters);
+        }
+    };
+    this.getResults = function() {
+        if (correctLetters.length === gameWord.length) {
+            feedbackContainer.css({
+                "color": "teal"
+            });
+            feedbackContainer.text("YOU WIN! The word is : " + gameWord);
+        } else if (strikes > 5) {
+            feedbackContainer.css({
+                "color": "maroon"
+            });
+            feedbackContainer.text("YOU LOSE! The word was: " + gameWord);
+        }
+    };
 }
 
 
-function guess(letter, word){
-  if(word.indexOf(letter) > -1){
-    feedbackContainer.css({"color":"blue"});
-    feedbackContainer.text("You got it:  " + letter);
-    correctLetters.push(letter);
-    displayBlanks();
-  }
-  else{
-    feedbackContainer.css({"color":"red"});
-    feedbackContainer.text("Nope, not:  " + letter);
-    wrongLetters.push(letter);
-    strikes += 1;
-    gallowsContainer.text(strikes);
-    guessedContainer.text(wrongLetters);
-  }
-}
-
-function getGameResults(){
-  if(correctLetters.length === gameWord.length){
-    feedbackContainer.css({"color":"teal"});
-    feedbackContainer.text("YOU WIN! The word is : " + gameWord);
-  }
-  else if(strikes > 5){
-    feedbackContainer.css({"color":"maroon"});
-    feedbackContainer.text("YOU LOSE! The word was: " + gameWord);
-  }
-}
 
 
-submitBtn.on('click', function(){
-  guess(letterContainer.val().toLowerCase(), gameWord);
-  getGameResults();
+game1 = new Game("ruby");
+
+submitBtn.on('click', function() {
+    game1.guess(letterContainer.val().toLowerCase(), game1.gameWord);
+    game1.getResults();
 });
-
